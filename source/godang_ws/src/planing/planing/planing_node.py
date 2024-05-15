c3e2b6583581afb453ecb72420911cadad069f4c
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2024-05-16 01:18:22 +0700

    add the node starter

---
+++ source/godang_ws/src/planing/planing/planing_node.py
@@ -0,0 +1,37 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class Planing(Node):
+
+    def __init__(self):
+        super().__init__('planing_node')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.01  # 100 hz
+
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'test'
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    planing = Planing()
+
+    rclpy.spin(planing)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    planing.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
