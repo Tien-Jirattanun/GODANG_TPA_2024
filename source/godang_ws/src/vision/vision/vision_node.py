5972a0277412e17d04b472eed4271195f551a2d7
Worakan Lasudee <78127819+19xx47@users.noreply.github.com>
2024-06-04 18:46:58 +0700

    Update vision ball node

--- source/godang_ws/src/vision/vision/vision_node.py
+++ source/godang_ws/src/vision/vision/vision_node.py
@@ -1,69 +1,118 @@
 from std_msgs.msg import Float32MultiArray
-from ultralytics import YOLO
-# from vision_class import DistanceCalculator
+from ultralytics import YOLOv10
 import rclpy
 from rclpy.node import Node
 import numpy as np
 import cv2
 
-# class DistanceCalculator:
-#     def __init__(self, focal_length, object_height):
-#         self.focal_length = focal_length  
-#         self.object_height = object_height  # Actual height of the object in meters
+class_names = ['purple', 'red']
 
-#     def calculate_distance(self, object_height_in_image):
-#         distance = (self.focal_length * self.object_height) / object_height_in_image
-#         return distance
+camera_matrix = np.array([[1029.138061543091, 0, 1013.24017],
+                          [0, 992.6178560916601, 548.550898],
+                          [0, 0, 1]])
 
-#     def process_detections(self, results):
+dist_coeffs = np.array([ 0.19576996 ,-0.24765409, -0.00625207 , 0.0039396 ,  0.10282869])
+
+new_camera_matrix = np.array([[1.08832011e+03, 0.00000000e+00 ,1.02215651e+03],
+                                [0.00000000e+00,1.05041880e+03 ,5.39881529e+02],
+                                [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+roi = [0, 0, 1919, 1079]
+
+def detect_objects(frame):
+    model = YOLOv10("../../../../../models/bestv10_redball.pt")
+    results = model(frame)
+    detections = []
+    for bbox in results:
+        boxes = bbox.boxes
+        cls = boxes.cls.tolist()
+        xyxy = boxes.xyxy
+        conf = boxes.conf
+        for i, class_index in enumerate(cls):
+            class_name = class_names[int(class_index)]
+            x1, y1, x2, y2 = map(int, xyxy[i])
+            detections.append([x1, y1, x2, y2])
+    list_of_ball = [detections, conf, class_name]
+    return list_of_ball
+
+def image_to_robot_coordinates(u, v, depth):
+    fx = new_camera_matrix[0, 0]
+    fy = new_camera_matrix[1, 1]
+    cx = new_camera_matrix[0, 2]
+    cy = new_camera_matrix[1, 2]
+
+    x_norm = (u - cx) / fx
+    y_norm = (v - cy) / fy
+    depth_T = depth + 0.215
+    xyz_robot_coordinates = [x_norm, y_norm, depth_T]
+    return xyz_robot_coordinates
+    
+
+def computeBallPosRobotframe(list_of_ball):
+    ## radio of the ball
+    radio_threshold = 1.2
+    ## check if there are any balls
+    if len(list_of_ball) == 0:
+        return None
+    ## check if there is any red balls
+    red_ball = False
+    for i in range(len(list_of_ball)):
+        if list_of_ball[i][2] == 'red':
+            red_ball = True    
+            break
+    
+    ## if there is no red ball
+    if red_ball == False:
+        return 999
+
+    ## first choose most confident ball and match radio
+    sorted_conf_ball = sorted(list_of_ball, key=lambda x: x[1], reverse=True)
+    for i in range(len(sorted_conf_ball)):
+        diff_x = sorted_conf_ball[i][0][2] - sorted_conf_ball[i][0][0]
+        diff_y = sorted_conf_ball[i][0][3] - sorted_conf_ball[i][0][1]
+        if sorted_conf_ball[i][2] == 'red' and diff_x/diff_y < radio_threshold:
+            ## compute the center of the ball
+            x1, y1, x2, y2 = sorted_conf_ball[i][0]
+            u = x1 + (x2 - x1) / 2
+            v = y1 + (y2 - y1) / 2
+
+            ## compute the image_to_robot_coordinates
+            X, Y, Z = image_to_robot_coordinates(u, v)
+            ball_pos = [X, Y, Z]
+
+            return ball_pos
+        
+def R2WConversion(ball_pos,robot_position_in_world_position):
+    x_r, y_r, theta_r = robot_position_in_world_position
+    theta_r = np.deg2rad(theta_r)
+    transformation_matrix = np.array([[np.cos(theta_r), -np.sin(theta_r), x_r],
+                                        [np.sin(theta_r), np.cos(theta_r), y_r],
+                                        [0, 0, 1]])
+    
+    X, Y, Z = ball_pos
+    robot_coords_homogeneous = np.array([Z, -X, 1])
+    world_coords_homogeneous = np.dot(transformation_matrix, robot_coords_homogeneous)
+    theta_w = np.rad2deg((theta_r))
+    return world_coords_homogeneous[0], world_coords_homogeneous[1], theta_w
+
+    
+
+
+
+            
+        
+
+
+    
+
+
+    
+        
        
-#         distances = []
-#         for bbox in results:
-#             xmin, ymin, xmax, ymax = bbox
-#             object_height_in_image = ymax - ymin
-#             distance = self.calculate_distance(object_height_in_image)
-#             distances.append(distance)
-#         return distances
-# xmax
+        
 
 
-def BoxToPos(x,y,w,h):
-   # camera center
-   Cx = 1022.625
-   Cy = 543.287
-   fx = 1074.764
-   fy = 1029.2567
-   u_x = x + 0.5*w
-   u_y = y + 0.5*h
-   approx_x = (u_x-Cx)/fx
-   approx_y = (u_y-Cy)/fy
-   ball_diameter = 0.25 # m NEED TO BE UPDATED
-   # is this Zc or distance??
-   Zc = 0.5*(ball_diameter/w*fx + ball_diameter/h*fy)
-   Zc_2 = np.sqrt(Zc*Zc/(1+approx_x*approx_x+approx_y*approx_y))
-   Xc = approx_x * Zc
-   Yc = approx_y * Zc
-   Xc2 = approx_x * Zc_2
-   Yc2 = approx_y * Zc_2
-#    tx = 0
-   ty = -0.47
-   tz = 0.24
-   print('X~', Xc, ' Z~', -ty - Yc) # should be close to ball radius
-   print('X2~', Xc2, ' Z~', -ty - Yc2) # should be close to ball radius
-   # camera mounting offset
-   return (Xc, Zc+tz)
-#    camera_mat = np.ze((3,3))
-#    camera_mat[0,0] = fx
-#    camera_mat[1,1] = fy
-#    camera_mat[2,2] = 1
-#    camera_mat[0,2] = Cx
-#    camera_mat[1,2] = Cy
-#    cam_inv = np.linalg.inv(camera_mat)
-#    rot_C2R = np.matrix([1,0,0], [0,1,0], [0,0,1])
-#    trans = np.array([tx,ty,tz])
-#    uv = np.array([x+0.5*w, y+0.5*h])
-#    res = Scaling_factor*cam_inv*uv - trans
-#    return (res[0], res[2])
+    
+
 
 def GetBallPositions(results):
     class_names = ['purple', 'red']
